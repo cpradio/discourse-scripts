d7a71633e020b72ea3d3c215d015c034d0d73645
cpradio <forums@cpradio.net>
2015-10-11 17:05:15 -0400

    Output folder before calling git pull command

--- update-discourse.sh
+++ update-discourse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/discourse/plugins/
-find . -maxdepth 1 -mindepth 1 -type d -exec sh -c 'cd $0; git pull;' {} \;
+find . -maxdepth 1 -mindepth 1 -type d -exec sh -c 'cd $0; echo $0; git pull;' {} \;
 cd ~/discourse
 bundle install
 bundle exec rake db:migrate
