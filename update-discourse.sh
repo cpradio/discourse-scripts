99e61b059830b0044925913e83df7ededddc1754
cpradio <forums@cpradio.com>
2016-05-03 08:18:02 -0400

    Added the ability to seed/prepare the test database

--- update-discourse.sh
+++ update-discourse.sh
@@ -4,4 +4,5 @@ find . -maxdepth 1 -mindepth 1 -type d -exec sh -c 'cd $0; echo $0; git pull;' {
 cd ~/discourse
 bundle install
 bundle exec rake db:migrate
+bundle exec rake db:migrate db:test:prepare db:seed_fu
 rm -Rf tmp/cache/assets
